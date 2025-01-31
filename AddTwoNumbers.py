5811218aee002946e8d54549e88c70be627ddfe4
emilifro <142464531+emilifro@users.noreply.github.com>
2025-01-31 13:42:03 +0100

    Added tasks for case in interview

---
+++ AddTwoNumbers.py
@@ -0,0 +1,14 @@
+# Task 2 - Add two numbers 
+# 
+# You are given two non-empty linked lists representing two non-negative integers. The digits 
+# are stored in reverse order, and each of their nodes contains a single digit. Add the two 
+# numbers and return the sum as a linked list. You may assume the two numbers do not contain 
+# any leading zero, except the number 0 itself
+# When finished stage the file and run GreenCheck
+
+def addTwoNumbers(self,l1,l2):
+    """
+    :type l1: Optional[ListNode]
+    :type l2: Optional[ListNode]
+    :rtype: Optional[ListNode]
+    """
